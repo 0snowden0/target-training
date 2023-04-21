248ec81ada4b38641c28989d608064c795a8c1b8
Prakash <prakashchidambaram2001@gmail.com>
2023-04-21 20:36:19 +0530

    initial commit

---
+++ DAY 01/intellij-maven-project/src/main/java/com/targetindia/programs/VariablesDemo.java
@@ -0,0 +1,17 @@
+package com.targetindia.programs;
+
+public class VariablesDemo {
+    public static void main(String[] args){
+        byte b1;
+        short s1 = 102;
+        int i1 = 494;
+        long l1;
+        String myName = "Prakash";
+        System.out.println("My Name is "+myName);
+
+        b1 = 100;
+
+        System.out.println("b1 = "+b1);
+
+    }
+}
